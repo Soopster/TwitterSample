d6cd667d5267ab16e6b11e1aad8fb1c3963a5d8a
Luke Ryan <luke.ryan@gmail.com>
2013-07-29 10:37:38 -0700

    Added acceptance tests

---
+++ TwitterSample.Tests/Acceptance/Steps/TweetStreamSteps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+using TwitterSample.Services;
+
+namespace TwitterSample.Tests
+{
+    [Binding]
+    public class TweetStreamSteps
+    {
+        private int _tweets;
+
+        [Given]
+        public void Given_I_have_an_API_key_for_twitter()
+        {
+            var accessToken = "2148771-lAyLM5At75ZLhDLNCQ8ijH0YrrvX4ntrSnuv1ySoPg";
+            var accessTokenSecret = "yBQPXj9bH2NpJ6Y0ns2cHd5l8lL5G5C00JUTm5A9KcU";
+            var consumerKey = "O44DgOFQ7Ps8jyQMU3SWsA";
+            var consumerKeySecret = "3PwT7czs7xnxjvgH3laPFz1kMlWCxfQWkwr10YQ";
+
+            var twitterAuthService = new TwitterAuthService(accessToken, accessTokenSecret, consumerKey,
+                consumerKeySecret);
+
+            var twitterService = new TwitterService(twitterAuthService, null);
+            ScenarioContext.Current.Set(twitterService);
+        }
+
+        private AutoResetEvent _autoReset = new AutoResetEvent(false);
+
+        [When]
+        public void When_I_call_the_service_with_the_following_Twitter_Ids(Table table)
+        {
+            var twitterService = ScenarioContext.Current.Get<TwitterService>();
+
+            foreach (var tableRow in table.Rows)
+            {
+                var id = tableRow["Id"];
+                twitterService.GetTimeLineByIdAsync(id).ContinueWith(x =>
+                {
+                    _tweets = x.Result.Count;
+                    _autoReset.Set();
+                });
+            }
+            
+        }
+        
+        [Then]
+        public void Then_tweets_should_be_returned()
+        {
+            _autoReset.WaitOne();
+            Assert.IsTrue(_tweets > 0);
+        }
+    }
+}
---
+++ TwitterSample.Tests/Acceptance/TweetStream.feature
@@ -0,0 +1,14 @@
+﻿Feature: TweetStream
+	As a user
+	I want to see my tweet stream
+	So i can keep up to date with my tweets
+
+@integration
+Scenario: Can call twitter to retrieve tweets
+	Given I have an API key for twitter
+	When I call the service with the following Twitter Ids
+	| Id            |
+	| pay_by_phone  |
+	| PayByPhone    |
+	| PayByPhone_Uk |
+	Then tweets should be returned
